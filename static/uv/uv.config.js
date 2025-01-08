b96ddf05d30c8712caa33cde8073bee8c3c97bec
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2025-01-07 22:20:18 -0500

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,10 +1,10 @@
 self.__uv$config = {
-    prefix: 'tiw/',
+    prefix: '/static/learn/',
     bare:'https://bare.benrogo.net',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: '/public/static/uv/uv.handler.js',
-    bundle: '/public/static/uv/uv.bundle.js',
-    config: '/public/staticuv/uv.config.js',
+    handler: '/static/uv/uv.handler.js',
+    bundle: '/static/uv/uv.bundle.js',
+    config: '/staticuv/uv.config.js',
     sw: '/static/uv/uv.sw.js',
 };
